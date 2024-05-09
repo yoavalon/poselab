66ad4128f6cb66c6da2ef36dbb727b820f69fdb2
Yoav Alon <31308918+yoavalon@users.noreply.github.com>
2024-05-09 18:07:06 +0300

    Add files via upload

---
+++ posefilter/__init__.py
@@ -0,0 +1,3 @@
+__version__ = "0.1.0"
+__author__ = 'Stephen Hudson'
+__credits__ = 'Argonne National Laboratory'
---
+++ posefilter/reader.py
@@ -0,0 +1,7 @@
+import numpy as np 
+
+def read() : 
+    print("this is the reading function")
+
+def write() : 
+    print("Writer !!")
